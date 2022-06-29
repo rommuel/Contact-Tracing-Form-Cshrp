af9632e6420a17045f1e8af1bbf3311e365660dc
rommuel <rommuelrivera3@gmail.com>
2022-06-29 16:32:03 +0800

    Managed to make the sorting function work and display the group of data in a message box. Will be applying the same concept to the date sorting as well.

--- Contact Tracing Form/Form2.cs
+++ Contact Tracing Form/Form2.cs
@@ -21,32 +21,29 @@ namespace Contact_Tracing_Form
         {
 
 
-            StreamReader reader = new StreamReader(@"C:\Users\PC\source\repos\ContactTracingForm\DataInput.txt");
-            reader.ReadToEnd();
-
-            List<string> Names = new List<string>();
+            StreamReader reader = new StreamReader(@"C:\Users\PC\source\repos\Contact Tracing Form\Data Input.txt");
+            
+            List<string> Names_Grouped = new List<string>();
 
             //Sort by Name
             string Surname = SNtxtbx.Text;
             string FirstName = FNtxtbx.Text;
             while (!reader.EndOfStream)
             {
-                string namedata = reader.ReadLine();
-                if (namedata.Contains(Surname))
+                string filteredname = reader.ReadLine();
+                if (filteredname.Contains(Surname))
                 { 
-                    Names.Add(namedata);
-                }
+                    MessageBox.Show(filteredname); 
+                    Names_Grouped.Add(filteredname);
+                }   
+            }
             reader.Close();
-            StreamWriter file = new StreamWriter(@"C:\Users\PC\source\repos\ContactTracingForm\SortedDataView.txt");
-                
-            foreach (string sorteddata in Names)
 
+            StreamWriter file = new StreamWriter(@"C:\Users\PC\source\repos\Contact Tracing Form\Sorted Data Viewer.txt");
+            foreach (string name in Names_Grouped)
             {
-                MessageBox.Show(sorteddata);
-                file.WriteLine(sorteddata);
-                file.Close();
-            }
-            }
+                MessageBox.Show(name);
+            }   
         }
 
         private void Timelbl_Click(object sender, EventArgs e)
